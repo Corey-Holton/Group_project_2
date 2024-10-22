589421f8eda2b00200d63ecba054932e8420c1b3
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2024-10-21 22:56:22 -0500

    Fixed issue where RSI was incorrect. Updated zip files on raw_data folder

--- utilities/stock_indicators.py
+++ utilities/stock_indicators.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 # Define RSI function
-def calculate_rsi(data, window=14):
-    delta = data.diff()
-    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
-    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
-    rs = gain / loss
+def calculate_rsi(historical_prices, window=14):
+    # Ensure calculations are performed on the 'Adj Close' column
+    delta = historical_prices.diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    avg_gain = gain.rolling(window=window, min_periods=1).mean()
+    avg_loss = loss.rolling(window=window, min_periods=1).mean()
+    rs = avg_gain / avg_loss
     rsi = 100 - (100 / (1 + rs))
     return rsi
 
